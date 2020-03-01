30380f75e4cca90ec6aaec124b457680f2216662
firman <firmansyah9dhe@gmail.com>
2020-03-02 00:24:21 +0700

    crud detail order

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const orders = require('./orders');
+const orderdetails = require('./orderdetails');
 const Router = express.Router();
 
 Router.use('/orders', orders);
+Router.use('/orders/detail', orderdetails);
 
 module.exports = Router;
