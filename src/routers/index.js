7429d5a38aee7189fea5a0b82af175c3f24810b3
raisy <firmansyah9dhe@gmail.com>
2020-03-02 16:50:37 +0700

    route user

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,13 @@ const orders = require('./orders');
 const laundry = require("./laundry");
 const features = require("./features");
 const orderdetails = require('./orderdetails');
+const user = require('./user');
 const Router = express.Router();
 
 Router.use('/orders', orders);
 Router.use('/orders/detail', orderdetails);
 Router.use("/laundry", laundry);
 Router.use("/features", features);
+Router.use('/user', user);
 
 module.exports = Router;
